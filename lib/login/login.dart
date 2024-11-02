65f6e16c0d957ccbc22b39310693b73d5eebeb63
enardostefanuszx <stefanus.enardo05@gmail.com>
2024-11-02 18:36:54 +0700

    Login and register

---
+++ lib/login/login.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:uas_flutter/constants.dart';
+import 'package:uas_flutter/login/signup.dart';
+import 'package:uas_flutter/size_config.dart';
+import 'package:uas_flutter/textField.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+  static String routeName = "/loginScreen";
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
+    return Scaffold(
+      backgroundColor: AppConstants.clrBackground,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          // Wrap the Container with SingleChildScrollView
+          child: Container(
+            margin: EdgeInsets.only(
+              left: getProportionateScreenWidth(15),
+              right: getProportionateScreenWidth(15),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: getProportionateScreenHeight(20),
+                ),
+                SvgPicture.asset(
+                  AppConstants.imgAppLogo,
+                  width: getProportionateScreenWidth(100),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(30),
+                ),
+                Text(
+                  AppConstants.signInTop,
+                  style: TextStyle(
+                    color: AppConstants.clrBlack,
+                    fontSize: getProportionateScreenWidth(30),
+                    fontFamily: AppConstants.fontInterBold,
+                    letterSpacing: -1,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(10),
+                ),
+                Text(
+                  AppConstants.enterEmailPassText,
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: getProportionateScreenWidth(11),
+                    fontFamily: AppConstants.fontInterRegular,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(30),
+                ),
+                TextFieldWidget(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  title: AppConstants.email,
+                  hintText: 'Enter your email here',
+                  textInputAction: TextInputAction.next,
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(20),
+                ),
+                TextFieldWidget(
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  title: AppConstants.password,
+                  obscure: true,
+                  hintText: 'Enter your password here',
+                  textInputAction: TextInputAction.done,
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(10),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      AppConstants.forgotPassword,
+                      style: TextStyle(
+                        color: AppConstants.textBlue,
+                        fontSize: getProportionateScreenWidth(14),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(20),
+                ),
+                Container(
+                  width: getProportionateScreenWidth(350),
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: AppConstants.mainColor,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: Center(
+                    child: Text(
+                      AppConstants.login,
+                      style: TextStyle(
+                        fontFamily: AppConstants.fontInterMedium,
+                        color: AppConstants.clrBackground,
+                        fontSize: getProportionateScreenWidth(16),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(20),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 150,
+                      height: 0.5,
+                      color: Colors.grey[400],
+                      margin: EdgeInsets.only(right: 10),
+                    ),
+                    Text(
+                      AppConstants.or,
+                      style: TextStyle(
+                        fontFamily: AppConstants.fontInterMedium,
+                        color: AppConstants.greyColor4,
+                        fontSize: getProportionateScreenWidth(13),
+                      ),
+                    ),
+                    Container(
+                      width: 150,
+                      height: 0.5,
+                      color: Colors.grey[400],
+                      margin: EdgeInsets.only(left: 10),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(20),
+                ),
+                // Add Google login button
+                _buildSocialLoginButton(
+                    AppConstants.imgGoogle, AppConstants.googleLogin),
+                SizedBox(height: getProportionateScreenHeight(20)),
+                // Add Facebook login button
+                _buildSocialLoginButton(
+                    AppConstants.imgFacebook, AppConstants.facebookLogin),
+                SizedBox(height: getProportionateScreenHeight(20)),
+                GestureDetector(
+                  onTap: () =>
+                      Navigator.pushNamed(context, SignUpScreen.routeName),
+                  child: Center(
+                    child: RichText(
+                      text: TextSpan(
+                        text: AppConstants.dontHaveAccount,
+                        style: TextStyle(color: AppConstants.clrBlack),
+                        children: [
+                          TextSpan(text: " "),
+                          TextSpan(
+                            text: AppConstants.signUp,
+                            style: TextStyle(color: AppConstants.textBlue),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSocialLoginButton(String image, String text) {
+    return Container(
+      width: getProportionateScreenWidth(350),
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        border: Border.all(
+          width: 1,
+          color: AppConstants.greyColor1,
+        ),
+      ),
+      child: Row(
+        children: [
+          SizedBox(width: getProportionateScreenWidth(15)),
+          SvgPicture.asset(image, width: getProportionateScreenWidth(25)),
+          SizedBox(width: getProportionateScreenWidth(15)),
+          Text(
+            text,
+            style: TextStyle(
+              fontFamily: AppConstants.fontInterMedium,
+              color: AppConstants.clrBlack,
+              fontSize: getProportionateScreenWidth(16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/login/signup.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:uas_flutter/constants.dart';
+import 'package:uas_flutter/size_config.dart';
+import 'package:uas_flutter/textField.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  static String routeName = "/signUpScreen";
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController fullNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController dobController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isPassword = false;
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
+    return Scaffold(
+      backgroundColor: AppConstants.clrBackground,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          // Fix overflow with SingleChildScrollView
+          child: Container(
+            margin: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(15)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: getProportionateScreenHeight(20),
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(20),
+                ),
+                Text(
+                  AppConstants.signUp,
+                  style: TextStyle(
+                    color: AppConstants.clrBlack,
+                    fontSize: getProportionateScreenWidth(30),
+                    fontFamily: AppConstants.fontInterBold,
+                    letterSpacing: -1,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(10),
+                ),
+                Text(
+                  AppConstants.createAccountText,
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: getProportionateScreenWidth(13),
+                    fontFamily: AppConstants.fontInterRegular,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(10),
+                ),
+                TextFieldWidget(
+                  controller: fullNameController,
+                  keyboardType: TextInputType.text,
+                  title: AppConstants.fullName,
+                  hintText: 'Enter full name',
+                  textInputAction: TextInputAction.next,
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(10),
+                ),
+                TextFieldWidget(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  title: AppConstants.email,
+                  hintText: 'Enter your email',
+                  textInputAction: TextInputAction.next,
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(10),
+                ),
+                TextFieldWidget(
+                  controller: dobController,
+                  keyboardType: TextInputType.datetime,
+                  title: AppConstants.dateOfBirth,
+                  hintText: 'ex. 06/09/2005',
+                  textInputAction: TextInputAction.next,
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(10),
+                ),
+                TextFieldWidget(
+                  controller: phoneNumberController,
+                  keyboardType: TextInputType.phone,
+                  title: AppConstants.phoneNumber,
+                  hintText: 'ex. 089623244972',
+                  textInputAction: TextInputAction.next,
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(10),
+                ),
+                TextFieldWidget(
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  title: AppConstants.password,
+                  hintText: 'Enter your password here',
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isPassword = !isPassword;
+                      });
+                    },
+                    child: Icon(
+                      isPassword ? Icons.visibility : Icons.visibility_off,
+                      color: AppConstants.clrBlack,
+                    ),
+                  ),
+                  obscure: isPassword ? true : false,
+                  textInputAction: TextInputAction.done,
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(20),
+                ),
+                Container(
+                  width: getProportionateScreenWidth(340),
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: AppConstants.mainColor,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: Center(
+                    child: Text(
+                      AppConstants.signUp,
+                      style: TextStyle(
+                          fontFamily: AppConstants.fontInterMedium,
+                          color: AppConstants.clrBackground,
+                          fontSize: getProportionateScreenWidth(16)),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(20),
+                ),
+                GestureDetector(
+                  onTap: () =>
+                      Navigator.pushNamed(context, '/login'), // Adjust route
+                  child: Center(
+                    child: RichText(
+                      text: TextSpan(
+                          text: AppConstants.alreadyHaveAccount,
+                          style: TextStyle(color: AppConstants.clrBlack),
+                          children: [
+                            TextSpan(
+                              text: " ",
+                            ),
+                            TextSpan(
+                              text: AppConstants.login,
+                              style: TextStyle(color: AppConstants.textBlue),
+                            )
+                          ]),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

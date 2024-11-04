dfbad5a8dee7bf00ee544d96084731070cbce006
enardostefanuszx <stefanus.enardo05@gmail.com>
2024-11-04 21:15:39 +0700

    merge branch

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,6 +3,7 @@ import 'package:uas_flutter/Home/home_page.dart';
 import 'package:uas_flutter/auth/login.dart';
 import 'package:uas_flutter/auth/signup.dart';
 import 'package:uas_flutter/Search/search_page.dart';
+import 'package:uas_flutter/Wishlist/WishlistPage.dart';
 import 'package:uas_flutter/settings/settings_page.dart';
 
 final Map<String, WidgetBuilder> routes = {

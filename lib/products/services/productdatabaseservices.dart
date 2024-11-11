85332775f338ea0ad2ccff896ca0ef13b95c77f2
230094Hengky <hengkywisianto@gmail.com>
2024-11-11 15:16:13 +0700

    benarkan lainnya

--- lib/products/services/productdatabaseservices.dart
+++ lib/products/services/productdatabaseservices.dart
@@ -79,20 +79,4 @@ class ProductDatabaseService {
       rethrow;
     }
   }
-
-    Future<List<Map<String, dynamic>>> fetchProductsByHarga(
-      String category) async {
-    try {
-      QuerySnapshot<Product> snapshot =
-          await _productsRef.where('Price', isEqualTo: category).get();
-      return snapshot.docs.map((doc) {
-        return {
-          'id': doc.id,
-          'product': doc.data(),
-        };
-      }).toList();
-    } catch (e) {
-      rethrow;
-    }
-  }
 }
